6feb5dd1b8e50aed125baf68de17733d4d7ecf08
Elmer Marquez <leomarqz.main@gmail.com>
2026-09-23 22:23:15 -0600

    se implemento el metodo para actualizar tareas

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -4,7 +4,7 @@ import express from 'express';
 const todos = [
     { id: 1, text: 'Buy milk', createdAt: new Date() },
     { id: 2, text: 'Buy bread', createdAt: new Date() },
-    { id: 3, text: 'Buy butter', createdAt: new Date() }
+    { id: 3, text: 'Buy butter', createdAt: null }
 ];
 
 export class TodosController {
@@ -34,11 +34,51 @@ export class TodosController {
 
     public createTodo = (req: express.Request, res: express.Response)=>{
 
-        const body = req.body;
+        const { text } = req.body;
+
+        if( !text ) return res.status(400).json({ error: 'Text property is required!' });
+
+        const newTodo = {
+            id: (todos.length + 1),
+            text: text,
+            createdAt: null
+        };
+
+        todos.push( newTodo );
 
         res.json({
-            message: 'TODO created!',
-            content: body
+            message: 'Task successfully created!',
+            data: newTodo
+        });
+
+    }
+
+    public updateTodo = (req: express.Request, res: express.Response)=>{
+
+        const id: number = +req.params.id;
+
+        if( isNaN(id) ) {
+            return res.status(400).json({ error: 'ID argument is not a number!' });
+        }
+
+        //! REFERENCIA | Cualquier actualizacion afecta al item en el arreglo.
+        const todo = todos.find( td => td.id === id );
+
+        if( !todo ){
+            return res.status( 404 ).json({ error: `Todo with id ${id} not found!` });
+        }
+
+        const { text } = req.body;
+
+        if( !text ){
+            return res.status(400).json({ error: 'Text property is required!' });
+        }
+
+        todo.text = text;
+
+        res.json({
+            message: 'Task successfully updated',
+            data: todo
         });
     }
 
